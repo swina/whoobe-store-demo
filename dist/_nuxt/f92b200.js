d4ae7cf6e1f236b70ed7bc1d1ecb25fe0de8ae67
Swina Allen <swina.allen@gmail.com>
2021-08-18 12:17:53 +0300

    Fixed facet routing

--- dist/_nuxt/f92b200.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{258:function(t,c,o){"use strict";o.r(c);c.default={buy:"Acquista",add_to_cart:"Aggiungi al carrello",checkout:"Vai alla cassa",detail:"Dettaglio"}}}]);
