2c9bc6fd902cec37a50228ca162c5e8ace6ce3ea
Swina Allen <swina.allen@gmail.com>
2021-09-14 17:22:00 +0300

    Fixed products filter

---
+++ dist/_nuxt/0b31b4f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{241:function(t,c,o){"use strict";o.r(c);c.default={en:{products:"Products",buy:"Buy",add_to_cart:"Add to cart",checkout:"Checkout",detail:"Detail"},it:{products:"Prodotti",buy:"Acquista",add_to_cart:"Aggiungi al carrello",checkout:"Vai alla cassa",detail:"Dettaglio"}}}}]);
--- dist/_nuxt/420377a.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{240:function(t,c,o){"use strict";o.r(c);c.default={en:{products:"Products",buy:"Buy",add_to_cart:"Add to cart",checkout:"Checkout",detail:"Detail"},it:{products:"Prodotti",buy:"Acquista",add_to_cart:"Aggiungi al carrello",checkout:"Vai alla cassa",detail:"Dettaglio"}}}}]);
